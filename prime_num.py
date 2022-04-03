dcd3b17f001f70cb57435064acafb27cbea089b9
geetakumri <geet.choti1@gmail.com>
2022-04-03 07:19:36 +0200

    code

---
+++ prime_num.py
@@ -0,0 +1,34 @@
+ # prime prime by Sieve of Eratosthenes
+
+import math
+import time
+
+
+def count_primes(num_range):
+
+    # Mark all numbers as prime
+    list_numbers = num_range * [True]
+
+    # Cross out 0, 1 as they are not primes
+    list_numbers[0] = list_numbers[1] = False
+
+    square_root = int(math.sqrt(num_range))
+    count = 0
+    for p in range(square_root):
+        if list_numbers[p] == True:
+            for i in range(p * p, num_range, p):
+                # Cross out non primes by marking them false
+                list_numbers[i] = False
+
+    for p in range(len(list_numbers)):
+        if list_numbers[p] == True:
+            count += 1
+    return count
+
+
+start_time = time.time()
+print(f" prime number between 0 and 1000000000000 : {count_primes(100000000)}")
+
+print(f"time for execution is: {time.time() - start_time} ")
+
+# list_numbers = num_range * [True] , MemoryError  for greater than 9 zeros
